5b71cdc59f42c5d6c64855e65009d049309afa30
ShuangWW <2660631784@qq.com>
2018-09-13 09:29:33 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="hello">{{msg}}</div>
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld',
+    data() {
+        return {
+            msg: 'Vue父子组件生命周期顺序'
+        }
+    },
+    beforeMout(){
+        console.log('this is child beforeMout')
+    },
+    mouted(){
+        console.log('this is child mouted')
+    },
+    beforeCreate(){
+        console.log('this is child beforeCreate')
+    },
+    created(){
+        console.log('this is child created')
+    },
+    beforeUpdate(){
+        console.log('this is child beforeUpdate')
+    },
+    updated(){
+        console.log('this is child updated')
+    },
+    beforeDestroy(){
+        console.log('this is child beforeDestroy')
+    },
+    destroyed(){
+        console.log('this is child destroyed')
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+    height: 60px;/*px*/
+    line-height: 60px;/*px*/
+    width: 750px;
+    font-size: 32px;/*px*/
+    border: 1px solid red;/*no*/
+    text-align: center;
+}
+</style>
---
+++ src/pages/pageOne.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="pageOne">
+        <hello-world></hello-world>
+    </div>
+</template>
+<script>
+import HelloWorld from '@/components/HelloWorld'
+export default {
+    name:'PageOne',
+    components:{
+        HelloWorld
+    },
+    beforeMout(){
+        console.log('this is parents beforeMout')
+    },
+    mouted(){
+        console.log('this is parents mouted')
+    },
+    beforeCreate(){
+        console.log('this is parents beforeCreate')
+    },
+    created(){
+        console.log('this is parents created')
+    },
+    beforeUpdate(){
+        console.log('this is parents beforeUpdate')
+    },
+    updated(){
+        console.log('this is parents updated')
+    },
+    beforeDestroy(){
+        console.log('this is parents beforeDestroy')
+    },
+    destroyed(){
+        console.log('this is parents destroyed')
+    }
+
+}
+</script>
+
